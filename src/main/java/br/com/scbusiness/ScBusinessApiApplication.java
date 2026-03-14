9ff31b383885158f887ff6754837edcfc58a8e2f
MatheusPerger <matheusperger@gmail.com>
2026-03-14 20:02:15 -0300

    feat: initial project setup with Spring Boot

---
+++ src/main/java/br/com/scbusiness/ScBusinessApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.scbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScBusinessApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScBusinessApiApplication.class, args);
+    }
+}
