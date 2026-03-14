7b0858d684c2d586f56c9dcd972e922ecaa67c66
MatheusPerger <matheusperger@gmail.com>
2026-03-14 20:18:08 -0300

    feat: add seed data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO business (name, owner_name, city, segment, contact, status, description, created_at, updated_at)
+VALUES
+    ('TechNova',
+     'Matheus Perger',
+     'Florianópolis',
+     'TECHNOLOGY',
+     'matheus@technova.com.br',
+     'ACTIVE',
+     'Empresa focada em soluções com IA',
+     NOW(),
+     NOW()
+    );
